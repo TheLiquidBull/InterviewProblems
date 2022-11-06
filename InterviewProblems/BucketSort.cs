7f50ec3344dee8bef363ed9a65bc914c208cdc4c
dhasuda <davihasuda@gmail.com>
2022-11-05 18:53:12 -0700

    BucketSort

---
+++ InterviewProblems/BucketSort.cs
@@ -0,0 +1,44 @@
+﻿namespace InterviewProblems;
+public static class BucketSort
+{
+    public static List<Person> Sort(List<Person> input)
+    {
+        // The keys are people's ages
+        Dictionary<int, List<Person>> buckets = new Dictionary<int, List<Person>>();
+
+        foreach(Person p in input)
+        {
+            if (!buckets.TryGetValue(p.age, out _))
+            {
+                buckets[p.age] = new List<Person>();
+            }
+
+            buckets[p.age].Add(p);
+        }
+
+        List<Person> result = new List<Person>();
+
+        // We assume all ages are in range [0, 1000[
+        for (int i=0; i<1000; i++)
+        {
+            if (buckets.TryGetValue(i, out List<Person> list))
+            {
+                result.AddRange(list);
+            }
+        }
+
+        return result;
+    }
+}
+
+public struct Person
+{
+    public readonly int age;
+    public readonly string name;
+
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+}
---
+++ InterviewProblemsTests/BucketSortTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using InterviewProblems;
+
+namespace InterviewProblemsTests;
+
+[TestClass]
+public class BucketSortTests
+{
+    [TestMethod]
+    public void OutputMustBeSortedByAge()
+    {
+        // Arrange
+        Person person1 = new Person("Alice", 30);
+        Person person2 = new Person("Bob", 25);
+        Person person3 = new Person("Charlie", 31);
+        List<Person> input = new List<Person>
+        {
+            person1,
+            person2,
+            person3,
+        };
+        
+        // Act
+        List<Person> output = BucketSort.Sort(input);
+
+        // Assert
+        List<Person> expected = new List<Person>
+        {
+            person2,
+            person1,
+            person3,
+        };
+
+        output.Should().BeEquivalentTo(expected);
+    }
+}
